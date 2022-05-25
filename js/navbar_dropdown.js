56c27dd15e19a0461da529b3cdae63131edb6030
NODUF <NitikVideos@gmail.com>
2022-05-25 16:54:04 +0200

    0.4

--- js/navbar_dropdown.js
+++ js/navbar_dropdown.js
@@ -1,50 +1,37 @@
-$("#nav_products").mouseenter(function(){
-    $('.drop').remove();
-
+$("#nav_services").mouseenter(function(){
     var dropData = 
     '<section class="drop">'+
-        '<p class="title">PRODUCTS</p>'+
-        '<p class="text"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>'+
-    '</section>';
-
-
-    $('.dropdown_body').append(dropData);
-});
-
-$("#nav_use-cases").mouseenter(function(){
-    $('.drop').remove();
-
-    var dropData = 
-    '<section class="drop">'+
-        '<p class="title">CASES</p>'+
-        '<p class="text"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>'+
-    '</section>';
-
-
-    $('.dropdown_body').append(dropData);
-});
-
-$("#nav_developers").mouseenter(function(){
-    $('.drop').remove();
-    
-    var dropData = 
-    '<section class="drop">'+
-        '<p class="title">LINKS</p>'+
-        '<a href="#" class="text">Steven Rose<span class="arrow"></span></a>'+
+        '<p class="title">SERVICES</p><br>'+
+        '<main class="list">'+
+        '<ul class="dropdown-list">'+
+            '<li><img class="icon" src="../img/icons/3D_square.png"> </img><a href="#" class="title">3D Graphics<span class="arrow"></span><br><p class="text">Models, Animations...</p></a></li>'+
+            '<li><img class="icon" src="../img/icons/2D_square.png"> </img><a href="#" class="title">2D Graphics<span class="arrow"></span><br><p class="text">Logos, Banners...</p></a></li>'+
+        '</ul>'+
+        '<ul class="dropdown-list">'+
+            '<li><img class="icon" src="../img/icons/video_editing.png"> </img><a href="#" class="title">Video Editing<span class="arrow"></span><br><p class="text">Commercial, Motion Graphics...</p></a></li>'+
+            '<li><img class="icon" src="../img/icons/code.png"> </img><a href="#" class="title">Coding<span class="arrow"></span><br><p class="text">HTML, CSS, JS...</p></a></li>'+
+        '</ul>'+
+        '</main>'+
     '</section>';
 
     $('.dropdown_body').append(dropData);
 });
 
-$("#nav_company").mouseenter(function(){
-    $('.drop').remove();
-
+$("#nav_about").mouseenter(function(){
     var dropData = 
     '<section class="drop">'+
-        '<p class="title">COMPANY</p>'+
-        '<p class="text"> Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>'+
+        '<p class="title">ABOUT</p><br>'+
+        '<p class="text">My name is Steven, I have been doing graphic design for 6 years.<br> 3D graphics, models, 2D UI and UX, banners ... </p><br>'+
+        '<a href="#" class="text">Show More<span class="arrow"></span></a>'+
     '</section>';
 
-
     $('.dropdown_body').append(dropData);
+});
+
+$("#nav_services").mouseleave(function(){
+    $('.drop').remove();
+});
+
+$("#nav_about").mouseleave(function(){
+    $('.drop').remove();
 });
